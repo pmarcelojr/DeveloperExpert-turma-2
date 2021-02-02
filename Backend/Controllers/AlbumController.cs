c9aebe4b24113edf81a739e7970f58f5a54a2d3b
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-02 17:52:17 -0300

    Implementation AlbumController

--- Backend/Controllers/AlbumController.cs
+++ Backend/Controllers/AlbumController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Model;
+using Backend.Repository;
 using Microsoft.AspNetCore.Mvc;
 //using Backend.Models;
 
@@ -11,20 +13,42 @@ namespace Backend.Controllers
     [ApiController]
     public class AlbumController : ControllerBase
     {
-        public AlbumController()
+        private AlbumRepository Repository { get; set; }
+
+        public AlbumController(AlbumRepository repository)
         {
+            Repository = repository;
         }
 
         [HttpGet("")]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
             // TODO: Your code here
-            return Ok(new
-            {
-                Id = Guid.NewGuid().ToString(),
-                Title = "Album legal",
-                Description = "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-            });
+            return Ok(await Repository.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAlbum(Guid id)
+        {
+            var result = await Repository.GetByIdAsync(id);
+
+            return result is not null ? Ok(result) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Album model)
+        {
+            await Repository.SaveAsync(model);
+
+            return Created($"/{model.Id}", model);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await Repository.GetByIdAsync(id);
+            await Repository.RemoveAsync(result);
+            return NoContent();
         }
     }
 }

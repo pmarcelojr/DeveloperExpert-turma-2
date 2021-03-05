5c3a3474868ce21c2b182f144cd40edd4d396bc2
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-03-05 19:42:02 -0300

    fix bug in Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -20,7 +20,7 @@ namespace Backend
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+		            webBuilder.UseStartup<Startup>();
                 });
     }
 }

b8eeec5619e93a452265999e9d8ac92a92343cc1
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-03-12 16:31:50 -0300

    starting implamentation login in frontEnd

--- FrontEnd/MusicApp/src/app/main/pages/authentication/login/login.component.ts
+++ FrontEnd/MusicApp/src/app/main/pages/authentication/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
 import { FuseConfigService } from "@fuse/services/config.service";
+import { PersistenceService } from "app/services/persistence.service";
+import { UserService } from "app/services/user.service";
 import swal from "sweetalert2";
 
 @Component({
@@ -15,7 +17,10 @@ export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     constructor(
         private fuseConfigService: FuseConfigService,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private service: UserService,
+        private router: Router,
+        private persistence: PersistenceService
     ) {
         this.fuseConfigService.config = {
             layout: {
@@ -40,5 +45,40 @@ export class LoginComponent implements OnInit {
             email: ["", [Validators.required, Validators.email]],
             password: ["", Validators.required],
         });
+    }
+
+    login(loginForm) {
+        if (loginForm.isValid == false) {
+            return;
+        }
+
+        let email = this.loginForm.get("email").value;
+        let password = this.loginForm.get("password").value;
+
+        this.service.authenticate({
+            email: email,
+            password: password
+        }).subscribe((data) => {
+            this.persistence.set("authenticate_user", data);
+            this.router.navigate(["/"]);
+            this.showMenus();
+        }, (error) => {
+            if (error.status == 422) {
+                swal.fire("Ops!", "Email ou senha invalido", "error");
+            }
+        })
+    }
+
+    showMenus() {
+        this.fuseConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: false,
+                },
+                toolbar: {
+                    hidden: false,
+                },
+            },
+        };
     }
 }
